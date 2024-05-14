54cd54ff271318836ab5758b7e0dd5bd0d2c0dc2
Diana Zapata <dianarocioczapata@gmail.com>
2024-05-14 15:26:11 -0600

    ios notifs added

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Diana Rocío Cerda Zapata on 14/05/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
